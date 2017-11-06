e29bd1df935293a7a71e87ea0d5f00330fb0d0b7
kantinee <kantinee@tot.co.th>
2017-11-07 00:58:36 +0700

    make it better

--- App.js
+++ App.js
@@ -100,7 +100,7 @@ app.post('/db/addFeedback', function (req, res, next) {
           // pass the error to the express error handler
           return next(err);
         }
-        //res.set('location', 'https://www.google.com');
+        res.set('location', 'https://www.google.com');
         //res.status(301).send();
         res.send(200);
         //res.redirect(301, 'http://1.179.246.105:8080/jubjai/landingPage/');
