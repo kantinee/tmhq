ad70702cc533e93322b4ca7fb7a981d5b6d48561
kantinee <kantinee@tot.co.th>
2017-11-07 00:06:00 +0700

    make it better

--- App.js
+++ App.js
@@ -74,13 +74,11 @@ app.post('/db/addFeedback', function(req,res){
 });*/
 
 app.post('/db/addFeedback', function (req, res, next) {
-    
-    
-    
+     
     var post_data = req.body;
 
     console.log(JSON.stringify(post_data));
-    
+    console.log(post_data.fName);
     var pg = require('pg');
     var conString = process.env.DATABASE_URL ||  "postgres://postgres:chatbot@localhost:5432/jubjai-bot-db";
     pg.connect(conString, function (err, client, done) {
@@ -88,7 +86,7 @@ app.post('/db/addFeedback', function (req, res, next) {
         // pass the error to the express error handler
         return next(err);
       }
-      client.query('INSERT INTO feedback (fbid,age) VALUES ($1, $2 );', [post_data.fName,  post_data.age], function (err, result) {
+      client.query('INSERT INTO feedback (fbid,age,sex,edu,job,email,qcb1,qcb2,qcb3,qcb4,goodcb,badcb,qq1,qq2,qq3,qq4,goodq,badq,qa1,qa2,qacomment) VALUES ($1, $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19, $20,$21 );', [post_data.fName,  post_data.age, post_data.sex ,post_data.edu,post_data.job, post_data.email, post_data.qcb1,post_data.qcb2, post_data.qcb3, post_data.qcb4, post_data.goodcb,post_data.badcb,post_data.qq1, post_data.qq2, post_data.qq3, post_data.qq4, post_data.goodq, post_data.badq, post_data.qa1,post_data.qa2,post_data.qacomment], function (err, result) {
         done(); //this done callback signals the pg driver that the connection can be closed or returned to the connection pool
   
         if (err) {
