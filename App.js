45c4f7247612f01cc15809c59c5f6646d783d5de
kantinee <kantinee@tot.co.th>
2017-11-07 09:58:24 +0700

    make it better

--- App.js
+++ App.js
@@ -103,8 +103,8 @@ app.post('/db/addFeedback', function (req, res, next) {
         }
         //res.set('location', 'https://www.google.com');
         //res.status(301).send();
-        res.send(200);
-        //res.redirect(301, 'http://1.179.246.105:8080/jubjai/landingPage/');
+        //res.send(200);
+        return res.redirect(301, 'http://1.179.246.105:8080/jubjai/landingPage/');
         // res.next();
         
       });
